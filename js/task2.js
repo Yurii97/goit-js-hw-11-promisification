cf956b7e5587c6c3f35ce73cfc24800aa37673b4
Yurii Onats <yura.onats@gmail.com>
2021-10-22 20:45:10 +0300

    task2

--- js/task2.js
+++ js/task2.js
@@ -5,12 +5,13 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
+const toggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map(user =>
     user.name === userName ? { ...user, active: !user.active } : user,
   );
+return new Promise(resolve => { logger(updatedUsers) })
 
-  callback(updatedUsers);
+//   callback(updatedUsers);
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
@@ -18,11 +19,11 @@ const logger = updatedUsers => console.table(updatedUsers);
 /*
  * Сейчас работает так
  */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
 
 /*
  * Должно работать так
  */
-// toggleUserState(users, 'Mango').then(logger);
-// toggleUserState(users, 'Lux').then(logger);
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
